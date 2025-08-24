ca5cb601f9288975b9108de9482351ae477e5ea0
Andrey Vavilov <a@vavilov.dev>
2025-08-24 17:56:32 +0200

    Refactor main function to include error handling and graceful exit process

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,13 @@
-console.error('Server started');
-// Quit the application
-process.exit(0);
+async function main() {
+    console.error('Server started');
+    // Quit the application
+    process.exit(0);
+}
+
+main().catch(err => {
+    console.error(err); 
+    process.exit(1);
+});
+
+process.on("SIGINT", () => process.exit(0));
+process.on("SIGTERM", () => process.exit(0));
